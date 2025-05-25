40520d40e4e497326c449e3c3afd56ab4998537a
suhana2005 <suhananishanth@gmail.com>
2025-05-25 18:17:46 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (52.52%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        for (int price : prices) {
+            if (price <= min1) {
+                min2 = min1;
+                min1 = price;
+            } else if (price < min2) {
+                min2 = price;
+            }
+        }
+        int totalCost = min1 + min2;
+        return totalCost > money ? money : money - totalCost;
+    }
+}
