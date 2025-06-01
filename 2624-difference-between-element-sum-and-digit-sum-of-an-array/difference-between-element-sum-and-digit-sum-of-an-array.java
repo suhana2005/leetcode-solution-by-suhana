3e5526f9ac94be786ae0dfcc859596fb114decad
suhana2005 <suhananishanth@gmail.com>
2025-06-01 18:57:55 +0530

    Time: 2 ms (98.97%) | Memory: 45.2 MB (21.59%) - LeetSync

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.java
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.java
@@ -1,18 +1,19 @@
 class Solution {
     public int differenceOfSum(int[] nums) {
-        int elementSum = 0;
-        int digitSum = 0;
+        int elementsum = 0;
+        int digitsum = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            elementSum = elementSum + nums[i];
+        for (int i = 0; i < nums.length; i++){
+        elementsum = elementsum + nums[i];
 
-            int current = nums[i];
-            while (current > 0) {
-                digitSum = digitSum + current % 10;
-                current = current / 10;
-            }
+        int current = nums[i];
+        while(current > 0)
+        {
+            digitsum = digitsum + current % 10;
+            current = current / 10;
         }
 
-        return Math.abs(elementSum - digitSum);
+        }
+        return Math.abs(elementsum - digitsum);
     }
 }
