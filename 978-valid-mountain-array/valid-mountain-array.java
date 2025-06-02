9425264738890b82bddf5b27efbbf5004ef5cd68
suhana2005 <suhananishanth@gmail.com>
2025-06-02 16:13:50 +0530

    Time: 2 ms (71.85%) | Memory: 45.4 MB (59.59%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.java
+++ 978-valid-mountain-array/valid-mountain-array.java
@@ -3,14 +3,14 @@ class Solution {
         if (arr.length < 3)
             return false;
         int i = 0;
-        while (i < arr.length - 1 && arr[i] < arr[i + 1]) { // checking it is increasing and it find the peak value
+        while (i < arr.length - 1 && arr[i] < arr[i + 1]) {
             i++;
         }
         if (i == 0 || i == arr.length - 1){
         return false;
         }
     
-        while (i < arr.length - 1 && arr[i] > arr[i + 1]) { // checking it is decreasing after the peak value 
+        while (i < arr.length - 1 && arr[i] > arr[i + 1]) { 
              i++;
             }
             return (i == arr.length - 1);
