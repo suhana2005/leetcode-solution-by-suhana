b340f2e557ef90d7df2182d5c990b5ef5da8f529
suhana2005 <suhananishanth@gmail.com>
2025-06-09 09:41:54 +0530

    Time: 47 ms (58.11%) | Memory: 91.8 MB (89.62%) - LeetSync

--- 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.java
+++ 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.java
@@ -1,33 +1,20 @@
-class Solution {
+ class Solution {
     public long interchangeableRectangles(int[][] rectangles) {
-        HashMap<String, Long> ratioMap = new HashMap<>();
+        
+        HashMap<Double, Long> map = new HashMap<>();
         long count = 0;
-
-        for (int[] rect : rectangles) {
-            int width = rect[0];
-            int height = rect[1];
-            int gcd = gcd(width, height);
-
-            String key = (width / gcd) + "," + (height / gcd);
-            count += ratioMap.getOrDefault(key, 0L);
-
-            ratioMap.put(key, ratioMap.getOrDefault(key, 0L) + 1);
+		
+        for (int[] A : rectangles) {
+		
+            double x = A[0];
+            double y = A[1];
+            double division = (double) x / y;
+			
+            if (map.containsKey(division)) {
+                count += map.get(division);
+            }
+            map.put(division, map.getOrDefault(division, 0L) + 1);
         }
-
         return count;
-    }
-    private int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
-
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        int[][] rectangles = {{4, 8}, {2, 4}, {4, 8}, {1, 2}};
-        System.out.println(sol.interchangeableRectangles(rectangles));  // Output should be 4
     }
 }
